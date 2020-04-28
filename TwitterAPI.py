c29d529991f71a6ab0652252268ee477b0e1b0a8
Shubham <12shubham1@gmail.com>
2020-04-28 01:07:41 +0100

    venv, working twitter API pull

--- TwitterAPI.py
+++ TwitterAPI.py
@@ -2,52 +2,124 @@
 Author: Shubham Bhatnagar
 Date Created: 25/04/2020
 
-Purpose: Twitter API interaction script
+Purpose: Twitter API interaction script using python-twitter 
+documentation: https://python-twitter.readthedocs.io/en/latest/index.html 
 
+Issues:
+
+1) The geot-tagging of tweets is not functional anymore (twitter deprectted)
+the functionality.
+- SOLN: New method is going to be trying to search for key terms related to 
+country
 """
 
 #%%
 # =============================================================================
 # Imports
 # =============================================================================
-import requests
+import twitter
+import pycountry
+import os
+
+# Loading in environment variables from .env file
+from dotenv import load_dotenv
+load_dotenv()
 
 #%%
 # =============================================================================
 # Creating connection to Twitter API
 # =============================================================================
 
+# Authentication codes
+ck = os.environ.get('CONSUMER_KEY')
+cks = os.environ.get('CONSUMER_KEY_SECRET')
+ak = os.environ.get('ACCESS_KEY')
+aks = os.environ.get('ACCESS_KEY_SECRET')
+
+# Authorizing access Oauth 1a (although could probably use 2)
+api = twitter.Api(consumer_key=ck,
+                  consumer_secret=cks,
+                  access_token_key=ak,
+                  access_token_secret=aks)
+
+
+#%%
 # =============================================================================
-# url = "https://twitterbukativ1.p.rapidapi.com/search"
-# 
-# ck = 'YH3XF8MuFe0SZIprvqZsnjjC3'
-# cks = 'SDayFNOWBS3HY34wKLlXLSX2DGwjOKrGmZfjPMqVWvMOCKHwRt'
-# ak = '741422330-wh3NrXKkwicE5RhbEvsvbJvsORWLZuDhanYoAlZk'
-# aks = 'yYzY8iXS5owbjlGMRiASxa34ZueTFIKwbJtWcYuZRMS6q'
-# q = ''
-# 
-# payload = "consumerKey={}&query={}&accessTokenKey={}&consumerSecret={}&accessTokenSecret={}".format(ck,q,ak,cks,aks)
-# headers = {
-#     'x-rapidapi-host': "TwitterBukatiV1.p.rapidapi.com",
-#     'x-rapidapi-key': "19a7424cc1mshe06401f1418a19bp1fd5d9jsn148caffdc24e",
-#     'content-type': "application/x-www-form-urlencoded"
-#     }
-# 
-# response = requests.request("POST", url, data=payload, headers=headers)
-# 
-# print(response.text)
+# Create search parameters using Pycountry
+# =============================================================================
+# List of countries we want to search through
+country_list=['germany','china','uk','spain','italy','singapore','hong kong',
+              'us']
+
+# Creating a list of all countries/states and their codes
+countries = pycountry.countries
+states  =pycountry.subdivisions
+
+# Mapping country code to country (identifying any errors)
+country_code = {}
+error = []
+
+for c in country_list:
+    try:
+        country_code[c] = countries.lookup(c).alpha_2
+    except:
+        error.append(c)
+
+print('These countries did not work: ',error)
+        
+# Mapping state name to country code
+
+# empty dictionary to store country, code list
+country_state= {}
+
+# Iterating through every country
+for k,v in country_code.items():
+    curr_states = list(states.get(country_code=v)) # all states for curr 
+    
+    temp = []
+    temp.append(k)
+    
+    for c in curr_states:
+        temp.append(c.name)    
+    
+    country_state[k] = temp
+    
+
+# Function that creates a list of search terms    
+def search_creator(country_state_list,term):
+    
+    cs = country_state_list.copy()
+    
+    for k,v in cs.items():
+        v = [term+ ' ' + x for x in v]
+        cs[k] = v
+
+    return cs        
+
+search = search_creator(country_state,'covid')
+
+
+
+
+#%%
+# =============================================================================
+# Searching for tweets
 # =============================================================================
 
-api = 'https://api.twitter.com/1.1/search/tweets.json'
+results = []
+for t in search['germany']:
+    results.append(api.GetSearch(term=t,lang='en',count=100,
+                                 result_type='mixed'))
+    
+#%%
+# =============================================================================
+# Printing results
+# =============================================================================
 
-url = 'https://twitter.com/search?q=employment&src=typed_query'
+count = 0
+for r in results:
+    for r2 in r:
+        print(r2.text + '\n')
+        count += 1
+        
 
-ck = 'YH3XF8MuFe0SZIprvqZsnjjC3'
-cks = 'SDayFNOWBS3HY34wKLlXLSX2DGwjOKrGmZfjPMqVWvMOCKHwRt'
-ak = '741422330-wh3NrXKkwicE5RhbEvsvbJvsORWLZuDhanYoAlZk'
-aks = 'yYzY8iXS5owbjlGMRiASxa34ZueTFIKwbJtWcYuZRMS6q'
-
-search_url = api+ url[url.find('?')::]
-
-payload = "consumerKey={}&accessTokenKey={}&consumerSecret={}&accessTokenSecret={}".format(ck,ak,cks,aks)
-r = requests.get(search_url, data = payload)
